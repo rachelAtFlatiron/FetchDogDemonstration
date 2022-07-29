13488b7560864243c6b1e4d944e80b0645d27def
Rachel Katz <rachel.katz@flatironschool.com>
2022-07-29 14:51:42 -0400

    catchEmAll function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('%c HI', 'color: firebrick')
+
+
+function catchEmAll(sldkjf){
+    lsdkjflskdjfslkdjflskdjf
+    sdlkfjlsdkjf
+    sdlkfjlsdkjffkljsklfj
+    sdlkfjsdkfjsdf
+    sldkjflsdkjflsdfw
+    iejf
+    
+}
